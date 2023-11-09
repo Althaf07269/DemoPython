a059a36daa186f7b093de7c3e4bd6c7d67d3d6bd
Althaf kb <135846367+Althaf07269@users.noreply.github.com>
2023-11-09 14:22:45 +0530

    Delete ecommerce_project directory

--- ecommerce_project/shop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name='shop'
-urlpatterns = [
-    path('',views.allProdCat,name='allProdCat'),
-    path('<slug:c_slug>/',views.allProdCat,name='prod_Cat'),
-    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='proDetail')
-]
